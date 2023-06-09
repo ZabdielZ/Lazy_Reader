a9be1b5174a675043b69a11c8f170a02b3d509c0
ZabdielZ <gm546161@gmail.com>
2023-06-09 15:14:30 -0600

    imagensn

---
+++ Pruebas2.py
@@ -0,0 +1,35 @@
+import os 
+p = os.path.getsize('PrincipeRana.pdf')
+print(p) 
+#if p == 940865:
+#    print(True)
+#else:
+#    print(False)
+
+
+
+
+
+
+
+#from gtts import gTTS
+#text='A  MARGARITA  DEBAYLE  Margarita está linda la mar  y el viento  lleva esencia sutil de azahar  yo siento  en el alma una alondra cantar  tu acento    Margarita te voy a contar  un cuento'  
+
+#tts = gTTS(text=text,lang='en-us')
+#tts.save('Hola.mp3')
+
+# from tkinter import *
+# from tkdocviewer import *
+
+# # Create a root window
+# root = Tk()
+
+# # Create a DocViewer widget
+# v = DocViewer(root)
+# v.pack(side="top", expand=1, fill="both")
+
+# # Display some document
+# v.display_file("Debayle.pdf")
+
+# # Start Tk's event loop
+# root.mainloop()
